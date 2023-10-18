8839a183ba89309cc3acc6aea04f32c027390ed1
Prasoon Shukla <prasoon.shukla@squareboat.com>
2023-10-18 13:53:55 +0530

    fix: rm my public key

--- react-native/app/config/imagekit.js
+++ react-native/app/config/imagekit.js
@@ -1,6 +1,6 @@
 // TODO
 const urlEndpoint = 'https://ik.imagekit.io/demo';
-const publicKey = 'public_YOILiat6AduTRbSWelI9rcb4I7k='; //insert your own public key here
+const publicKey = 'YOUR_PUBLIC_KEY'; //insert your own public key here
 const authenticationEndpoint = 'http://localhost:8080/auth'; //your auth api path, currently set to demo server included with this project
 
 module.exports.urlEndpoint = urlEndpoint;
